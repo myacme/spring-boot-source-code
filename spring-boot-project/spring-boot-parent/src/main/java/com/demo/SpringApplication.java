00b68b4d1c71a596288939d985d4e45ec7419316
liujixiang <1030375081@qq.com>
2022-06-29 10:27:08 +0800

    提交

---
+++ spring-boot-project/spring-boot-parent/src/main/java/com/demo/SpringApplication.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+
+/**
+ * 〈〉
+ *
+ * @author ljx
+ * @version 1.0.0
+ * @create 2022/6/28 14:55
+ */
+public class SpringApplication {
+
+
+}
---
+++ src/main/java/com/demo/Application.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+
+/**
+ * 〈〉
+ *
+ * @author ljx
+ * @version 1.0.0
+ * @create 2022/6/28 11:53
+ */
+
+public class Application {
+
+
+}
